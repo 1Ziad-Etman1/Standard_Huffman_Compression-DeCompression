74c91e7560e61563c94bb1b7bf54df898b921100
Ziad Emad <115287958+1Ziad-Etman1@users.noreply.github.com>
2023-11-09 22:09:23 +0200

    edited the compressor to calculate the frequencies

--- Standard_Huffman/src/Compressor.java
+++ Standard_Huffman/src/Compressor.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+
 public class Compressor {
+    public ArrayList<Integer> compress(String test){
+        //String test = "AAAAAABBBAABBBBCDCCC";
+
+        // We get the chars and the freq of each char
+        ArrayList<Character> chars = new ArrayList<>();
+        ArrayList<Integer> freq = new ArrayList<>();
+        for (int i = 0; i < test.length(); i++) {
+            if (!chars.isEmpty()){
+                boolean new_char = true;
+                for(int j=0 ; j<chars.size() ;j++){
+                    if (chars.get(j).equals(test.charAt(i))){
+                        new_char =false;
+                        freq.set(j,freq.get(j)+1);
+                        break;
+                    }
+                }
+                if (new_char){
+                    chars.addLast(test.charAt(i));
+                    freq.addLast(1);
+                }
+            }
+        }
+
+        // now we get the ratios between the freq and the population
+//        for (int i = 0; i < freq.size(); i++) {
+//            freq.set(i, freq.get(i)/test.length());
+//        }
+
+        
+
+
+
+        return new ArrayList<>();
+    }
 }
--- Standard_Huffman/src/Node.java
+++ Standard_Huffman/src/Node.java
@@ -1,4 +1,13 @@
 public class Node {
+    int val;
+    char c;
     tag left;
     tag right;
+
+    public Node() {
+        this.val = 0;
+        this.c = '=';
+        this.left = null;
+        this.right = null;
+    }
 }
